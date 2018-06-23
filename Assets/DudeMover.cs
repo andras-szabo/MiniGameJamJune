74007778e58c55079350cd0a51e7fce154e19237
andras <andras.szabo.fin@gmail.com>
2018-06-23 15:41:15 +0200

    initial commit

---
+++ Assets/DudeMover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeMover : MonoBehaviour
+{
+	public float moveFactor = 2f;
+
+	private void Update()
+	{
+		var dir = Input.GetAxis("Horizontal") * moveFactor * Time.deltaTime;
+		transform.Translate(new Vector3(dir, 0f, 0f));
+	}
+}
---
+++ Assets/ForceThing.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ForceThing : MonoBehaviour
+{
+	public Rigidbody2D rb;
+	public float forceFactor = 2f;
+	public bool applyConstantForce;
+	public Vector2 myConstantForce;
+	public float drunkLevel;
+	public float drunkImpulsePeriodSeconds;
+
+	private float _previousForce;
+
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Start()
+	{
+		if (applyConstantForce)
+		{
+			StartCoroutine(DrunkMoveRoutine());
+		}
+	}
+
+	void Update()
+	{
+		if (!applyConstantForce)
+		{
+			var force = Input.GetAxis("Horizontal") * forceFactor;
+			rb.AddForce(new Vector2(force, 0f));
+		}
+	}
+
+	private IEnumerator DrunkMoveRoutine()
+	{
+		while (true)
+		{
+			rb.AddForce(new Vector2(_previousForce, 0f));
+
+			var moveToRightForce = drunkLevel * myConstantForce.x * 0.5f;
+			var randomChange = GetRandomDrunkennessQuotient();
+			var finalForce = moveToRightForce + randomChange;
+			rb.AddForce(new Vector2(finalForce, 0f));
+
+			_previousForce = -finalForce;
+
+			yield return new WaitForSeconds(drunkImpulsePeriodSeconds);
+		}
+	}
+
+	private float GetRandomDrunkennessQuotient()
+	{
+		return Random.Range(-drunkLevel, drunkLevel) * myConstantForce.x * 1f;
+	}
+}
